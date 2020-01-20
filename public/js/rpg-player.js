15d9b3a26b0747c6805472ef39d7957548378f86
hara-chan <harachan657@gmail.com>
2020-01-20 23:43:32 +0900

    話表示、ページ分割まで完了、ページ毎にウエイト未実装

--- public/js/rpg-player.js
+++ public/js/rpg-player.js
@@ -48,6 +48,20 @@ var mainCharaDir = 'down';
 var currentMapImgWidth;
 //現在選択中マップ縦
 var currentMapImgHeight;
+//キャンバス描画用フラグ
+var drawFlg = true;
+//会話用ウィンドウスタート位置X
+var talkWinStartX = mapTipLength;
+//会話用ウィンドウスタート位置Y
+var talkWinStartY = viewCanvasHeight - (mapTipLength*5);
+//会話用ウィンドウ横幅
+var talkWinWidth = viewCanvasWidth - (mapTipLength*2);
+//会話用ウィンドウ縦幅
+var talkWinHeight = mapTipLength*4;
+//会話用フォント
+var talkFont = "32px monospace";
+
+
 
 
 //================================ 各種エレメント ===============================================//
@@ -217,6 +231,16 @@ function keyDownHandler(evt) {
                 mainCharaDir = scrollDir;
                 mainCharaImg =  mainCharaImgArray[3];
                 break;
+            
+            case 65: //Aボタン
+                //トリガーAボタンのチェック
+                var res = checkTrigger('Aボタン', scrollDir);
+                if (res != false) {
+                    doEvents(res);
+                }
+                return;
+                break;
+
             default:
                 //上記以外のキーは受け付けない
                 return;
@@ -273,6 +297,161 @@ function checkStartMoveEvent() {
     return true;
 }
 
+//トリガーをチェックする
+function checkTrigger(trigger, direction) {
+    switch (trigger) {
+        case 'Aボタン':
+            switch (direction) {
+                case 'left':
+                    //マップ外でないかチェック
+                    if (mainCharaPosX == 0) return false;
+                    //通りぬけかどうかチェック
+                    var nextCellY = mainCharaPosY/mapTipLength;
+                    var nextCellX = mainCharaPosX/mapTipLength-1;
+                    var nextCell = currrentMapObj[nextCellY][nextCellX];
+                    if(nextCell.hasOwnProperty('trigger')) {
+                        if(nextCell['trigger'] == 'Aボタン') {
+                            return nextCell['events'];
+                        }
+                    } else {
+                        return false;
+                    }
+                    break;
+                case 'up':  
+                    //マップ外でないかチェック 
+                    if (mainCharaPosY == 0) return false;
+                    //通りぬけかどうかチェック
+                    var nextCellY = mainCharaPosY/mapTipLength-1;
+                    var nextCellX = mainCharaPosX/mapTipLength;
+                    var nextCell = currrentMapObj[nextCellY][nextCellX];
+                    if(nextCell.hasOwnProperty('trigger')) {
+                        if(nextCell['trigger'] == 'Aボタン') {
+                            return nextCell['events'];
+                        }
+                    } else {
+                        return false;
+                    }
+                    break;
+                case 'right':  
+                    //マップ外でないかチェック 
+                    if (mainCharaPosX+mapTipLength == currentMapImgWidth) return false;
+                    //通りぬけかどうかチェック
+                    var nextCellY = mainCharaPosY/mapTipLength;
+                    var nextCellX = mainCharaPosX/mapTipLength+1;
+                    var nextCell = currrentMapObj[nextCellY][nextCellX];
+                    if(nextCell.hasOwnProperty('trigger')) {
+                        if(nextCell['trigger'] == 'Aボタン') {
+                            return nextCell['events'];
+                        }
+                    } else {
+                        return false;
+                    }
+                    break;
+                case 'down':
+                    //マップ外でないかチェック 
+                    if (mainCharaPosY+mapTipLength == currentMapImgHeight) return false;
+                    //通りぬけかどうかチェック
+                    var nextCellY = mainCharaPosY/mapTipLength+1;
+                    var nextCellX = mainCharaPosX/mapTipLength;
+                    var nextCell = currrentMapObj[nextCellY][nextCellX];
+                    if(nextCell.hasOwnProperty('trigger')) {
+                        if(nextCell['trigger'] == 'Aボタン') {
+                            return nextCell['events'];
+                        }
+                    } else {
+                        return false;
+                    }
+                    break;
+                default:
+                    return false;
+                    break;
+            }
+            break;
+    }
+}
+
+//マップチップに設定されたイベントを実行する
+function doEvents(maptipObj) {
+    Object.keys(maptipObj).forEach(function (key) {
+        //console.log("キー : " + key + ", 値 : " + maptipObj[key]);  
+        //イベントネームを取得
+        var evtName = key.substr(2);
+        switch (evtName) {
+            case 'talk':
+                var talkContent =  maptipObj[key]['talkContent'];
+                doTalk(talkContent);
+                //viewContext.drawImage(currentMapImg, viewCanvasHalfWidth-mainCharaPosX, viewCanvasHalfHeight-mainCharaPosY);
+                break;
+        }
+    });
+}
+
+function doTalk(talkContent) {
+    //drawを止める
+    drawFlg = false;
+    // var timerId = setTimeout("draw()", 1);
+    // clearTimeout(timerId);
+
+    //会話用ウィンドウスタート位置X
+    //var talkWinStartX = mapTipLength;
+    //会話用ウィンドウスタート位置Y
+    //var talkWinStartY = viewCanvasHeight - (mapTipLength*5);
+    //会話用ウィンドウ横幅
+    //var talkWinWidth = viewCanvasWidth - (mapTipLength*2);
+    //会話用ウィンドウ縦幅
+    //var talkWinHeight = mapTipLength*4;
+    //会話用フォント
+    //var talkFont = "14px monospace";
+
+    //会話ウィンドウを白でクリア
+    viewContext.fillStyle = 'white';
+    viewContext.fillRect(talkWinStartX, talkWinStartY, talkWinWidth, talkWinHeight);
+    //会話ウィンドウを黒でクリア
+    viewContext.fillStyle = 'black';
+    viewContext.fillRect(talkWinStartX+2, talkWinStartY+2, talkWinWidth-4, talkWinHeight-4);
+    //会話表示
+    viewContext.fillStyle = 'white';
+    viewContext.textBaseline = 'top';
+    viewContext.font = talkFont;
+    var lineLength = 0;
+    var lineMaxLength = talkWinWidth-20;
+    var lines = [];
+    var lineIndex = 0;
+    lines[lineIndex] = '';
+
+    talkContent = 'aaaaaaaauuuuuuuuuiiiiiiiiieeeeeeeehhhhhhhhhhbfjsbajbdjfbajdbfjabdjfbadfbjsbdfjsbfjabbdfajdbfjakdlfkjbakdjbflajbdklfabdklbflajdbfkajdbfkajdbfklbdajkfbdaklb';
+
+    for (var i=0; i<talkContent.length; i++) {
+        lines[lineIndex] += talkContent[i];
+        lineLength = viewContext.measureText(lines[lineIndex]);
+        if (lineLength.width > lineMaxLength-mapTipLength) {
+            lineIndex++;
+            lines[lineIndex] = '';
+        }
+    }
+
+    var pages = [];
+    var pageIndex = 0;
+    pages[pageIndex] = [];
+    for (var i=0; i<lines.length; i++) {
+        if (i != 0 && i%3 == 0) {
+            pageIndex++;
+            pages[pageIndex] = [];
+        }
+        pages[pageIndex].push(lines[i]);
+    }
+    for (var i=0; i<pages.length; i++) {
+        viewContext.fillStyle = 'white';
+        for (var j=0; j<pages[i].length; j++) {
+            viewContext.fillText(pages[i][j], talkWinStartX+2+10, talkWinStartY+2+10+(j*mapTipLength));
+        }
+        var test =0;
+        viewContext.fillStyle = 'black';
+        viewContext.fillRect(talkWinStartX+2, talkWinStartY+2, talkWinWidth-4, talkWinHeight-4);
+        //会話表示、ページ分割まで完了、ページ毎にウエイト実装未
+    }
+}
+
 function draw() {
     switch (scrollState) {
         //非スクロール中
@@ -311,11 +490,15 @@ function draw() {
         scrollPos = 0;
     }
 
-    viewContext.clearRect(0, 0, viewCanvasWidth, viewCanvasHeight);
-    viewContext.drawImage(currentMapImg, viewCanvasHalfWidth-mainCharaPosX, viewCanvasHalfHeight-mainCharaPosY);
-    drawMainCharacter();
-    setTimeout("draw()", 3);
-        
+    if (drawFlg) {
+        viewContext.clearRect(0, 0, viewCanvasWidth, viewCanvasHeight);
+        viewContext.drawImage(currentMapImg, viewCanvasHalfWidth-mainCharaPosX, viewCanvasHalfHeight-mainCharaPosY);
+        drawMainCharacter();
+        setTimeout("draw()", 3);
+    } else {
+        var timerId = setTimeout("draw()", 3);
+        clearTimeout(timerId);
+    }
 }
 
 
